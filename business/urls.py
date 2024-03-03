adf27231cc23506a624b35a78c1e305cec16fc14
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-03-03 21:51:50 +0500

    main page is ready

--- business/urls.py
+++ business/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin-get-access-1528/", admin.site.urls),
     path("", include("uzrent.urls")),
 ]
 
